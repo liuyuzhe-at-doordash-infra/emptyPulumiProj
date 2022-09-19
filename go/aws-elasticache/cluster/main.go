cb9776cbb5ca4ec1b054473894eaef7438ee6bfd
liuyuzhe-at-doordash-infra <yuzhe.liu@doordash.com>
2022-09-19 14:40:31 -0700

    Refactor with interfaces

--- go/aws-elasticache/cluster/main.go
+++ go/aws-elasticache/cluster/main.go
@@ -2,64 +2,157 @@ package main
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 	"time"
+
+	"github.com/pkg/errors"
 
 	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/elasticache"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
+const (
+	ResourceImportKey   = "is_import"
+	ResourceImportIdKey = "resource_import_id"
+	ResourceNameKey     = "name"
+)
+
+type MyPulumiInterface interface {
+	Myimport(string) pulumi.ResourceOption
+}
+
+type MyElasticacheInterface interface {
+	MyCreateReplicationGroup(*pulumi.Context, *config.Config, *elasticache.ReplicationGroupArgs, []pulumi.ResourceOption) (*elasticache.ReplicationGroup, error)
+}
+
+//func main() {
+//	pulumi.Run(func(ctx *pulumi.Context) error {
+//		cfg := config.New(ctx, "")
+//
+//		rgCfg, err := ConfigWithDefaults(ctx)
+//		if err != nil {
+//			return errors.Wrap(err, "error building config")
+//		}
+//
+//		opts := make([]pulumi.ResourceOption, 0)
+//
+//		isImport := cfg.GetBool(ResourceImportKey)
+//		if isImport {
+//			opts = append(opts, pulumi.Import(pulumi.ID(cfg.Require(ResourceImportIdKey))))
+//		}
+//
+//		rg, err := elasticache.NewReplicationGroup(ctx, cfg.Require(ResourceNameKey), rgCfg, opts...)
+//		if err != nil {
+//			return errors.Wrap(err, "error creating or importing replication group")
+//		}
+//
+//		ctx.Export("configuration_endpoint_address", rg.ConfigurationEndpointAddress)
+//		ctx.Export("engine_actual_version", rg.EngineVersionActual)
+//
+//		// Export current timestamp to force stack update
+//		ctx.Export("updatedAt", pulumi.StringPtr(fmt.Sprintf("%d", time.Now().Unix())))
+//
+//		return nil
+//	})
+//}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		//cfg := config.New(ctx, "")
+		cfg := config.New(ctx, "")
 
-		//isImport := cfg.GetBool("is_import")
-		isImport := true // force to test
-		if isImport {
-			fmt.Println("### Importing ElastiCache ###")
-
-			// import elasticache
-			importRgCfg, importErr := ConfigWithImport(ctx)
-			if importErr != nil {
-				return errors.Wrap(importErr, "error building config from imports")
-			}
-
-			importRg, importErr := elasticache.NewReplicationGroup(
-				ctx,
-				"imported-pulumi-stack-my-test-222-elasticache-redis-simple",
-				importRgCfg,
-				pulumi.Import(pulumi.ID("my-test-222-elasticache-redis-simple")),
-			)
-			if importErr != nil {
-				return errors.Wrap(importErr, "error importing the existing replication group")
-			}
-
-			fmt.Printf("importRg.ConfigurationEndpointAddress: %v", importRg.ConfigurationEndpointAddress)
-			//fmt.Printf("importRg.EngineVersionActual: %v", importRg.EngineVersionActual)
-		} else {
-			fmt.Println("### Creating ElastiCache ###")
-
-			// create elasticache
-			cfg := config.New(ctx, "")
-
-			rgCfg, err := ConfigWithDefaults(ctx)
-			if err != nil {
-				return errors.Wrap(err, "error building default config")
-			}
-
-			rg, err := elasticache.NewReplicationGroup(ctx, cfg.Require("name"), rgCfg)
-			if err != nil {
-				return errors.Wrap(err, "error creating replication group")
-			}
-
-			ctx.Export("configuration_endpoint_address", rg.ConfigurationEndpointAddress)
-			ctx.Export("engine_actual_version", rg.EngineVersionActual)
-
-			// Export current timestamp to force stack update
-			ctx.Export("updatedAt", pulumi.StringPtr(fmt.Sprintf("%d", time.Now().Unix())))
+		rgCfg, err := ConfigWithDefaults(ctx)
+		if err != nil {
+			return errors.Wrap(err, "error building config")
 		}
+
+		myPulumiInterface := &MyRealPulumi{}
+		myElasticacheInterface := &MyRealElasticache{}
+
+		rg, err := helper(ctx, cfg, rgCfg, myPulumiInterface, myElasticacheInterface)
+		if err != nil {
+			return errors.Wrap(err, "error creating or importing replication group")
+		}
+
+		ctx.Export("configuration_endpoint_address", rg.ConfigurationEndpointAddress)
+		ctx.Export("engine_actual_version", rg.EngineVersionActual)
+
+		// Export current timestamp to force stack update
+		ctx.Export("updatedAt", pulumi.StringPtr(fmt.Sprintf("%d", time.Now().Unix())))
 
 		return nil
 	})
+}
+
+type MyRealPulumi struct{}
+
+// MyRealPulumi struct implmenets MyPulumiInterface interface
+func (myRealPulumi *MyRealPulumi) Myimport(s string) pulumi.ResourceOption {
+	return pulumi.Import(pulumi.ID(s))
+}
+
+type MyRealElasticache struct{}
+
+// MyRealElasticache struct implmenets MyElasticacheInterface interface
+func (myRealElasticache *MyRealElasticache) MyCreateReplicationGroup(ctx *pulumi.Context, cfg *config.Config, rgCfg *elasticache.ReplicationGroupArgs, opts []pulumi.ResourceOption) (*elasticache.ReplicationGroup, error) {
+	rg, err := elasticache.NewReplicationGroup(ctx, cfg.Require(ResourceNameKey), rgCfg, opts...)
+	return rg, err
+}
+
+func playFunc(ctx *pulumi.Context) error {
+
+	cfg := config.New(ctx, "")
+
+	//rgCfg, err := ConfigWithDefaults(ctx)
+	rgCfg, err := ConfigWithImport(ctx)
+	if err != nil {
+		return errors.Wrap(err, "error building config")
+	}
+
+	myPulumiInterface := &MyRealPulumi{}
+	myElasticacheInterface := &MyRealElasticache{}
+
+	//opts := make([]pulumi.ResourceOption, 0)
+	//
+	//isImport := cfg.GetBool(ResourceImportKey)
+	//
+	//if isImport {
+	//	//opts = append(opts, pulumi.Import(pulumi.ID(cfg.Require(ResourceImportIdKey))))
+	//	opts = append(opts, myPulumiInterface.Myimport("myID"))
+	//}
+	//
+	////rg, err := elasticache.NewReplicationGroup(ctx, cfg.Require(ResourceNameKey), rgCfg, opts...)
+	//rg, err := myElasticacheInterface.MyCreateReplicationGroup("name", ctx, cfg)
+
+	rg, err := helper(ctx, cfg, rgCfg, myPulumiInterface, myElasticacheInterface)
+
+	if err != nil {
+		return errors.Wrap(err, "error creating or importing replication group")
+	}
+
+	ctx.Export("configuration_endpoint_address", rg.ConfigurationEndpointAddress)
+	ctx.Export("engine_actual_version", rg.EngineVersionActual)
+
+	// Export current timestamp to force stack update
+	ctx.Export("updatedAt", pulumi.StringPtr(fmt.Sprintf("%d", time.Now().Unix())))
+
+	return nil
+}
+
+func helper(ctx *pulumi.Context,
+	cfg *config.Config,
+	rgCfg *elasticache.ReplicationGroupArgs,
+	myPulumiInterface MyPulumiInterface,
+	myElasticacheInterface MyElasticacheInterface) (*elasticache.ReplicationGroup, error) {
+
+	opts := make([]pulumi.ResourceOption, 0)
+	isImport := cfg.GetBool(ResourceImportKey)
+
+	if isImport {
+		//opts = append(opts, pulumi.Import(pulumi.ID(cfg.Require(ResourceImportIdKey))))
+		opts = append(opts, myPulumiInterface.Myimport("myID"))
+	}
+
+	rg, err := myElasticacheInterface.MyCreateReplicationGroup(ctx, cfg, rgCfg, opts)
+
+	return rg, err
 }
